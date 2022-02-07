61d9ce640945c2d19b70a78f2690c484363f8280
jisooyu <jisooyu@hotmail.com>
2022-02-07 16:33:35 +0900

    authentication - register & login

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const connectDB = require('./config/db')
 const app = express();
+
+// connect DB
+connectDB()
 
 // Init Middleware
 app.use(express.json({ extended: false }));
 
+// define routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/users', require('./routes/users'));
 app.use('/api/camp', require('./routes/camp'))
 
 const PORT = process.env.PORT || 5000;
