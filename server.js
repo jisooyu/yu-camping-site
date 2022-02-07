a7049eae5649e0e1e675ffe00c9851c173ba922e
jisooyu <jisooyu@hotmail.com>
2022-02-07 15:35:09 +0900

    preparing of using auth and db by adding routes folder and camp.js in it

--- server.js
+++ server.js
@@ -1,21 +1,10 @@
 const express = require('express');
 const app = express();
 
-const fetchData = require('./campingsite/fetchData');
-
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', async (req, res) => {
-  try {
-    const response = await fetchData();
-    console.log(response.data.response.body.items);
-    res.send(response.data.response.body.items);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).send(error);
-  }
-});
+app.use('/api/camp', require('./routes/camp'))
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
